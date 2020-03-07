a428e60c2145814b411c8da2ed7cf5c4087fcfb8
Olega95 <o9818154818@yandex.ru>
2020-03-07 18:47:22 +0300

    Calculator project

--- src/ru/oleg/javarush/MetodCalc.java
+++ src/ru/oleg/javarush/MetodCalc.java
@@ -1,20 +1,32 @@
 package ru.oleg.javarush;
 
 public class MetodCalc {
-    public double calc(double n1, String op, double n2) {
-        double res = 0;
+    public Object calc(double n1, String op, double n2) {
+        Object res = 0;
         switch (op) {
             case "+":
-                res = n1 + n2;
+                if ((n1 + n2) % 1 == 0) {
+                    res = (int) (n1 + n2);
+                } else
+                    res = (double) (n1 + n2);
                 break;
             case "-":
-                res = n1 - n2;
+                if ((n1 - n2) % 1 == 0) {
+                    res = (int) (n1 - n2);
+                } else
+                    res = (double) (n1 - n2);
                 break;
             case "*":
-                res = n1 * n2;
+                if ((n1 * n2) % 1 == 0) {
+                    res = (int) (n1 * n2);
+                } else
+                    res = (double) (n1 * n2);
                 break;
             case "/":
-                res = n1 / n2;
+                if ((n1 / n2) % 1 == 0) {
+                    res = (int) (n1 / n2);
+                } else
+                    res = (double) (n1 / n2);
                 break;
             default:
                 res = 0;
